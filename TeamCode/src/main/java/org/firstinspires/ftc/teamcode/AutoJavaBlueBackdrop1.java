fb79a9df0a9c473298f0868f9ca3573838ccc3ed
Shalinda Ranasinghe <sranasinghe@kaleris.com>
2023-12-09 05:46:54 -0500

    missing break in the while loop added coments for moves refactored to armDrop drop1 drop2

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/AutoJavaBlueBackdrop1.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/AutoJavaBlueBackdrop1.java
@@ -3,12 +3,12 @@ package org.firstinspires.ftc.teamcode;
 
 import com.qualcomm.robotcore.eventloop.opmode.Autonomous;
 
-@Autonomous(name = "AutoJavaRedBackdrop", group = "Auto")
-public class AutoJavaRedBackdrop extends AutoJava {
+@Autonomous(name = "AutoJavaBlueBackdrop1", group = "Auto")
+public class AutoJavaBlueBackdrop1 extends AutoJava {
 
 
-    public AutoJavaRedBackdrop() {
-        super(false);
+    public AutoJavaBlueBackdrop1() {
+        super(true);
     }
 
 
@@ -34,95 +34,75 @@ public class AutoJavaRedBackdrop extends AutoJava {
         camera.closeCameraDevice();
         while (opModeIsActive()) {
 
-            // robot must be 8.5 inches away from the left of the robot
-            switch (pixelDetection.getPosition()) { //
+
+            switch (pixelDetection.getPosition()) {
 
                 case LEFT: {
 
-                    //moveBot(5 , 0, 0, -1);
-                    moveBot(30, 0, 0, 1); // ➜
-                    sleep(500);
-                    moveBot(83, 1, 0, 0); // ↑
-                    sleep(1500);
-                    turnBot(-95);                              // ↶
-                    sleep(500);
-                    // correct for robot length since it turns
-                    moveBot(13.62,1,0,0); // ↑
-                    sleep(500);
-
+                    moveBot(43, 0, 0, -1);// ←
                     armDrop();
-
-//                    moveBot(43, 0, 0, 1);
-                    moveBot(12, 1, 0, 0); // ↑
+                    moveBot(29, 1, 0, 0); // ↑
                     drop1();
 
-                    moveBot(20, -1, 0, 0);// ↓
-
-                    turnBot(230);                               // ↷
-                    moveBot(60, 1, 0, 0); // ↑
-                    moveBot(15, 0, 0, -1);// ←
+                    turnBot(-100);                             // ↶
+                    moveBot(80, 1, 0, 0); // ↑
+                    moveBot(15, 0, 0, 1); // ➜
                     moveBot(5, 1, 0, 0);  // ↑
+
                     drop2();
-                    sleep(1000);
-                    //moveBot(5, 1, 0, 0);
+
                     break;
 
                 }
 
+
                 case CENTER: {
 
-                    //moveBot(8, 0, 0, 1);
-                    moveBot(47, 1, 0, 0); // ↑
-
+                    moveBot(23, 0, 0, -1);// ←
                     armDrop();
-
-                    moveBot(20, 1, 0, 0); // ↑
-                    sleep(500);
-                    moveBot(5, -1, 0, 0); // ↓
+                    moveBot(67, 1, 0, 0); // ↑
                     drop1();
-                    turnBot(100);                              // ↷
+
+                    turnBot(-90);                              // ↶
                     sleep(1000);
                     moveBot(84, 1, 0, 0); // ↑
+                    moveBot(15, 0, 0, 1); // ➜
                     sleep(500);
-                    moveBot(15, 0, 0, -1);// ←
-                    //moveBot(15, 0, 0, -1);
-                    sleep(500);
-                    moveBot(10, 1, 0, 0); // ↑
+                    moveBot(15, 1, 0, 0); // ↑
+
                     drop2();
-                    //moveBot(5, 1, 0, 0);
+
                     break;
 
                 }
 
                 case RIGHT: {
 
-                    moveBot(63, 1, 0, 0); // ↑
-                    sleep(500);
+                    moveBot(15, 0, 0, -1);// ←
                     armDrop();
-                    moveBot(20, 1, 0, 0); // ↑
-                    turnBot(100);                               // ↷
-                    sleep(500);
+                    moveBot(83, 1, 0, 0); // ↑
+                    sleep(1000);
+                    turnBot(95);
 
-                    moveBot(5,-1,0,0);    // ↓
                     drop1();
 
-                    moveBot(90, 1, 0, 0); // ↑
+                    moveBot(2, -1, 0, 0); // ↓
                     sleep(500);
-                    moveBot(15, 0, 0, 1); // ➜
+                    turnBot(215);                              // ↷
+                    sleep(500);
+                    moveBot(35, 0, 0, 1); // ➜
+                    sleep(500);
+                    moveBot(90, 1, 0, 0); // ↑
+                    moveBot(5, 0, 0, 1);  // ➜
+                    moveBot(10, 1, 0, 0); // ↑
 
                     drop2();
-
                     break;
 
                 }
-
             }
             break;
-
-
         }
-
-
 
     }
 
